e136eb2956765b0a764e2ac7626b0ffb7ab9dea9
cleroux <cleroux84@gmail.com>
2020-06-10 21:06:53 +0200

    premiere page

---
+++ htdocs/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/style.css">
+  <title>COBATURAGE</title>
+</head>
+<body>
+
+  <nav>
+    <?php
+      include "./partials/nav.php";
+    ?>
+  </nav>
+  <header>
+      <?php
+        include "./partials/header.php"
+      ?>
+  </header>
+
+  <footer>
+      <?php
+       include "./partials/footer.php"
+      ?>
+  </footer>
+
+</body>
+</html>
